24a0166cbe96f882f7408e1112f238f062d9e37b
christabel samuel <christ4samgithub@gmail.com>
2015-11-23 11:32:05 +0000

    create new and edit forms and partials

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,71 @@
+class RecipesController < ApplicationController
+  
+  def index
+    @recipes = Recipe.all
+    
+  end
+  
+  def new
+    
+    @recipe = Recipe.new
+  end
+  # here create a new recipe and save, if you are able to save you will be redirected to list and a 
+  # flash message that recipe was created 
+  # of recipes but if cant save the create a new form again will be displayed again.
+  # with a chef object to ensure every recipe is created by a registered chef so each 
+  # recipe created is assigned to a chef
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(2)   #the chef object creating recipe
+    
+     if @recipe.save
+      flash[:success] = "Your recipe was created successfully!"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+    
+  end
+  #  to this point
+  
+  
+  
+  # here we are retriving a particular recipe we want to edit using the params which has the id
+  def edit
+    @recipe = Recipe.find(params[:id])
+    
+    
+  end
+  
+  # after the recipe is edited and submited we redirect user back to the edited 
+  #recipe show page else  we render the edit form again
+  def update
+    @recipe = Recipe.find(params[:id])
+   
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+      
+    else
+      render :edit
+      
+    end
+    
+  end
+  
+  
+  def show 
+    @recipe = Recipe.find(params[:id])
+    
+  end
+  
+  # here we are whitelisting or writting out the peramiters our app can permit to flow through
+  # for creating a new recipe with our new form 
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:name, :summary, :description)
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  
   root 'pages#home'
- get '/home', to: 'pages#home'
+  
+ # get '/home', to: 'pages#home' # will go to the home page will use the page controller home action
+ #get '/recipes', to: 'recipes#index' #will go to recipes page to list out all our recipes this is using recipe controller index action
+ #get '/recipes/new', to: 'recipes#new', as: 'new_recipe' #will get the form needed to create new recipe use recipes controller new action
+ # post '/recipes', to: 'recipes#create' #this will submit the form after filled to create a new recipe uses recipe controller create action
+ #get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe' #this will get the page for one recipe for editing recipe controller edit action
+ #patch '/recipes/:id', to: 'recipes#update' #this will submit the recipe back after editing recipe controller update
+ #get '/recipes/:id', to: 'recipes#show', as: 'recipe' #this will go to the page that shows one single recipe
+ #get '/recipes/:id', to: 'recipes#destroy' #this displays a singel recipe to be destroyed
+ 
+ 
+ resources :recipes
+
 end
