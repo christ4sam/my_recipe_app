1138679e5b4e5448cf7f016e8ec501fb014926cb
christabel samuel <christ4samgithub@gmail.com>
2015-11-24 14:33:40 +0000

    add image uploader

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,15 @@ class Recipe < ActiveRecord::Base
   validates :summary, presence: true, length: { minimum: 10, maximum: 150 }
   validates :description, presence: true, length: { minimum: 20, maximum: 500 }
   
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  # this server side validation ensures user can not upload image that is bigger than 5megabytes in size
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
+      
 end
